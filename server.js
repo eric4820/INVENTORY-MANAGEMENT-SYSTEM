470556efdb1844694b6e5440b495600269338555
eric4820 <erickoome2017@gmail.com>
2022-06-05 23:41:33 +0300

    commit a

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const mongoose = require('mongoose');
 const config = require('config');
@@ -6,6 +7,7 @@ const app = express();
 //body parsermiddlewere
 app.use(express.json());
 const db = config.get('mongoURL');
+NODE_ENV=production;
 mongoose
     .connect(db, {
         useUnifiedTopology: true,
@@ -22,5 +24,17 @@ app.use('/api/employees', require('./routes/api/employees'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/pauth', require('./routes/api/pauth'));
 
+// Serve frontend
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')))
+  
+    app.get('*', (req, res) =>
+      res.sendFile(
+        path.resolve(__dirname, '../', 'client', 'build', 'index.html')
+      )
+    )
+  } else {
+    app.get('/', (req, res) => res.send('Please set to production'))
+  }
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`SERVER STARTED ON PORT ${port}`));
